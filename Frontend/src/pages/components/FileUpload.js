3c26444653eba4eb2b6aafc562df8ee5c76db9cc
Khushi Tanwar <khushitanwar2201@gmail.com>
2024-12-31 02:53:05 +0530

    updated the url

--- Frontend/src/pages/components/FileUpload.js
+++ Frontend/src/pages/components/FileUpload.js
@@ -17,13 +17,19 @@ export default function FileUpload() {
 
       try {
         console.log(predictionModel);
-        const response = await fetch("http://127.0.0.1:5000/predict", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ image: base64Image, model: predictionModel }),
-        });
+        const response = await fetch(
+          "https://mlg-aat-production.up.railway.app/predict",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              image: base64Image,
+              model: predictionModel,
+            }),
+          }
+        );
 
         const data = await response.json();
         if (data.error) {
